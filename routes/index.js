7cd0f31d082b740f98e3af1b0a3916cc984fe79b
Charles F. Munat <charles.munat@gmail.com>
2015-06-26 23:23:30 +1200

    Basic email setup.

--- routes/index.js
+++ routes/index.js
@@ -1,4 +1,7 @@
 const passport   = require("koa-passport")
+const postmark   = require("postmark")
+const client     = new postmark.Client("<key>")
+const slugid     = require("slugid")
 
 let home = function *() {
   if (this.isAuthenticated()) {
@@ -29,11 +32,51 @@ let logout = function *() {
   this.redirect("/")
 }
 
+let getPostmarkThunk = (sender, receiver, subject, body) => {
+  return (callback) => {
+    client.sendEmail({
+      "From": sender,
+      "To": receiver,
+      "Subject": subject,
+      "TextBody": body
+    }, callback)
+  }
+}
+
+let reset = function *() {
+  let id = slugid.v4()
+  let sender = "<registered email>"
+  let subject = "[Quoth] Reset your credentials"
+  let body = "Someone requested to reset your Quoth credentials. To change them " +
+    "please follow this link: http://localhost:3000/reset/" + id
+  let receiver = this.request.body.fields.email
+
+  let result = yield getPostmarkThunk(sender, receiver, subject, body)
+
+  if (result) {
+    this.type = "application/json"
+    this.body = JSON.stringify(result)
+  } else {
+    this.status = 400
+  }
+}
+
+let resetForm = function *() {
+
+}
+
+let updatePassword = function *() {
+
+}
+
 export default function (router) {
   return function *(next) {
     router.get("/", home)
     router.post("/login", login)
     router.get("/logout", logout)
+    router.post("/reset", reset)
+    router.get("/reset/:id", resetForm)
+    router.put("/reset/:id", updatePassword)
     router.get("/:page", home)
 
     yield next
